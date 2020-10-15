0135889ebda47764ded188fbbaa85716d1411ba9
Ufuk Demir <38029528+ufukdemir54@users.noreply.github.com>
2020-10-15 15:54:09 +0300

    Core Commit

---
+++ NetCoreMvc/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreMvc.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [DisplayName("Full Name")]
+        public string FullName { get; set; }
+        public string EmpCode { get; set; }
+        public string Position { get; set; }
+        public string Location { get; set; }
+    }
+}
